a3c02e248277f2d0f166d9f1240812294704a132
杉山慶 <sugiyamakei@sugiyamakeinoMacBook-Pro.local>
2023-04-11 12:16:31 -0700

    Added another website

--- pages/home.tsx
+++ pages/home.tsx
@@ -16,7 +16,7 @@ function Home () {
             alt="Image of a bank app"
             link="https://kei-mybank.netlify.app"
             header="My bank"
-            description="[Temporaly slow. Please be patient]This app uses React, Node.js(Express), MongoDB, Firebase Authentication. It demonstrate my skill of creatig a secure app with RESTful API with MERN stack."
+            description="[Temporaly being slow. Please be patient]This app uses React, Node.js(Express), MongoDB, Firebase Authentication. It demonstrate my skill of creatig a secure app with RESTful API with MERN stack."
             />
             <Card
             link="#"
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,32 @@ function Home () {
     return (
         <Layout>
             <Card
-            src="/images/gmapTable.png"
-            alt="Image of a map app"
-            link="https://gmapstableviewer.netlify.app"
-            header="Google map tablizer"
-            description="This app let you search for multiple search results for places/directions at once. Made with Google maps api and React"
+                src="/images/hooked_magazine.png"
+                alt="Image of a magazine website"
+                link="https://hooked-magazine.netlify.app"
+                header="Cultural magazine"
+                description="This is a demo website made with Typescript, Next.js, SCSS. Fully responsive site with some animations."
             />
             <Card
-            src="/images/myBank.png"
-            alt="Image of a bank app"
-            link="https://kei-mybank.netlify.app"
-            header="My bank"
-            description="This app uses React, Node.js(Express), MongoDB, Firebase Authentication. It demonstrate my skill of creatig a secure app with RESTful API with MERN stack."
+                src="/images/gmapTable.png"
+                alt="Image of a map app"
+                link="https://gmapstableviewer.netlify.app"
+                header="Google map tablizer"
+                description="This app let you search for multiple search results for places/directions at once. Made with Google maps api and React"
             />
             <Card
-            src="/images/portfolio.png"
-            alt="Image of a portfolio website"
-            header="Portfolio"
-            description="This responsive website is built with Next.js and Typescript to demonstrate my frontend skills."
+                src="/images/myBank.png"
+                alt="Image of a bank app"
+                link="https://kei-mybank.netlify.app"
+                header="My bank"
+                description="This app uses React, Node.js(Express), MongoDB, Firebase Authentication. It demonstrate my skill of creatig a secure app with RESTful API with MERN stack."
+            />
+            <Card
+                src="/images/portfolio.png"
+                alt="Image of a portfolio website"
+                link=""
+                header="Portfolio"
+                description="This responsive website is built with Next.js and Typescript to demonstrate my frontend skills."
             />
         </Layout>
     )

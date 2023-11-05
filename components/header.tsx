7af41886a6465c88dee5caef36f8bbb2b7a5c26b
杉山慶 <sugiyamakei@sugiyamakeinoMacBook-Pro.local>
2023-11-05 08:55:03 -0800

    layout adjasted again

--- components/header.tsx
+++ components/header.tsx
@@ -5,11 +5,9 @@ export default function Header() {
     return (
         <article className={headerStyles.intro}>
             <h1>Kei Sugiyama</h1>
-            <p>Hi, I am Kei Sugiyama, a certifiled fullstack engeneer. <br/> I have been working as a translator/marketing writer for the past 8 years and look forward to
-bringing my language and marketing skills to my new career as a software developer. I
-graduated from MIT’s full stack web development certificate program and have a background in
-full stack development. <br/> I enjoys making websites that can make meaningful
-contributions to people’s lives.</p>
+            <p>Hi, I am Kei Sugiyama, a certifiled fullstack engeneer. I have been working as a translator/marketing writer for the past 8 years and look forward to
+                bringing my language and marketing skills to my new career as a software developer. I graduated from MIT’s full stack web development certificate program and have a background in
+                full stack development. I enjoys making websites that can make meaningful contributions to people’s lives.</p>
         </article>
     )
-  }
+}
